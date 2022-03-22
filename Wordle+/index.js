80a05db6ab378f2100333fcdcbf8425c08bceabd
shashmahawar <shshnkmhwr@gmail.com>
2022-03-22 14:54:33 +0530

    0_0

--- Wordle+/index.js
+++ Wordle+/index.js
@@ -1,16 +1,36 @@
 const http = require("http");
 
-const SECRET = "CIGAR"; // You can set any word as the secret answer
+const SECRET = "NGINX"; // You can set any word as the secret answer
 
 function myFunction(req, res) {
 	// console.log({req}); // You can uncomment this to see the request object
-	console.log(req.url);
-
-	const GUESS = ""; // Write logic to parse the word which the user guessed from the URL string
-	const feedback = ""; // Write logic to compare the word with the secret, and generate the feedback string
-
-	res.write(feedback);
-	res.end();
+	const tGUESS = req.url.split('q=')[1];
+	let GUESS = tGUESS.toUpperCase();
+	let GUESSLIST = GUESS.split('');
+	let SECRETLIST = SECRET.split('');
+	if (GUESS.length != 5) {
+		return res.end('Please enter a 5 letter word!');
+	}
+	for (i in GUESSLIST) {
+		if (GUESSLIST[i] === SECRETLIST[i]) {
+			GUESSLIST[i] = 'g';
+			SECRETLIST[i] = '0'
+		}
+	}
+	for (i in GUESSLIST) {
+		if (SECRETLIST.indexOf(GUESSLIST[i]) > -1) {
+			GUESSLIST[i] = 'y';
+			SECRETLIST[SECRETLIST.indexOf(GUESSLIST[i])] = '0';
+		}
+	}
+	for (i in GUESSLIST) {
+		if (GUESSLIST[i] != 'g' && GUESSLIST[i] != 'y') {
+			GUESSLIST[i] = 'b';
+		}
+	}
+	res.setHeader('Content-Type', 'text/plain');
+	
+	res.end(GUESSLIST.join(''));
 }
 
 http.createServer(myFunction).listen(8080);
